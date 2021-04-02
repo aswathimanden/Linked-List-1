011b79075e503c686edd9737d97f1d2d51ba78a1
aswathimanden <79280396+aswathimanden@users.noreply.github.com>
2021-04-01 22:48:01 -0400

    Create Problem_1.java

---
+++ Problem_1.java
@@ -0,0 +1,18 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes 
+// Any problem you faced while coding this : No
+
+class Solution {
+    
+    public ListNode reverseList(ListNode head) {
+        ListNode prev =null;
+        while(head != null){
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
